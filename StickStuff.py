575f7eddaa5d07507b999f8a5f3b755561603436
Pietdagamer <Pietdagamer@users.noreply.github.com>
2014-04-25 18:15:45 +0200

    Removed unnecessary lines

--- StickStuff.py
+++ StickStuff.py
@@ -4,6 +4,8 @@ from pygame.locals import *
 # --- Variables needed in classes ---
 frameTime = 0
 
+resizer = 4
+
 # --- Classes and functions ---
 
 def distance(speed, time):
@@ -15,13 +17,13 @@ class Stickman(object):
         self.position = position
         self.head = head
         self.hand = hand
-        self.stickmanRect = pygame.Rect(self.position[0], self.position[1], 16 * RESIZER, 32 * RESIZER)
-        self.destination = [random.randint(0, 1200 - (16 * RESIZER)), random.randint(0, 700 - (32 * RESIZER))]
+        self.stickmanRect = pygame.Rect(self.position[0], self.position[1], 16 * resizer, 32 * resizer)
+        self.destination = [random.randint(0, 1200 - (16 * resizer)), random.randint(0, 700 - (32 * resizer))]
         self.reached = [False, False]
         self.lastMoveTime = pygame.time.get_ticks()
         self.waitTime = random.randint(3000, 20000)
     def render(self):
-        self.stickmanRect = pygame.Rect(self.position[0], self.position[1], 16 * RESIZER, 32 * RESIZER)
+        self.stickmanRect = pygame.Rect(self.position[0], self.position[1], 16 * resizer, 32 * resizer)
         windowSurface.blit(baseImg, (self.position[0], self.position[1]))
         windowSurface.blit(self.head, (self.position[0], self.position[1]))
         windowSurface.blit(self.hand, (self.position[0], self.position[1]))
@@ -43,7 +45,7 @@ class Stickman(object):
             self.reached[1] = True
 
         if self.reached == [True, True] and pygame.time.get_ticks() - self.lastMoveTime >= self.waitTime:
-            self.destination = [random.randint(0, 1200 - (16 * RESIZER)), random.randint(0, 700 - (32 * RESIZER))]
+            self.destination = [random.randint(0, 1200 - (16 * resizer)), random.randint(0, 700 - (32 * resizer))]
             self.reached = [False, False]
             self.lastMoveTime = pygame.time.get_ticks()
             self.waitTime = random.randint(3000, 20000)
@@ -58,7 +60,6 @@ class Explosion(object):
             windowSurface.blit(explosion[self.frame], (self.position[0], self.position[1]))
         except IndexError:
             return False
-        print pygame.time.get_ticks() - self.lastFrameTime
         if pygame.time.get_ticks() - self.lastFrameTime >= 30:
             self.frame = self.frame + 1
             self.lastFrameTime = pygame.time.get_ticks()
@@ -72,8 +73,6 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-RESIZER = 4
-
 WINDOWWIDTH = 1200
 WINDOWHEIGHT = 700
 
@@ -86,35 +85,35 @@ basicFont = pygame.font.SysFont(None, 23)
 mainClock = pygame.time.Clock()
 
 # --- Image loading ---
-baseImg = pygame.transform.scale(pygame.image.load('baseman.png'), (16 * RESIZER, 32 * RESIZER))
-emptyImg = pygame.transform.scale(pygame.image.load('empty.png'), (16 * RESIZER, 32 * RESIZER))
+baseImg = pygame.transform.scale(pygame.image.load('baseman.png'), (16 * resizer, 32 * resizer))
+emptyImg = pygame.transform.scale(pygame.image.load('empty.png'), (16 * resizer, 32 * resizer))
 
-hatList =  [pygame.transform.scale(pygame.image.load('empty.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('cap.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('beard.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('beardGray.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('monocle.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('cowboy.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('hat.png'), (16 * RESIZER, 32 * RESIZER)),]
+hatList =  [pygame.transform.scale(pygame.image.load('empty.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('cap.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('beard.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('beardGray.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('monocle.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('cowboy.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('hat.png'), (16 * resizer, 32 * resizer)),]
 
-handList = [pygame.transform.scale(pygame.image.load('empty.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('stick.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('spear.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('beer.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('pokeball.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('hammer.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('pickaxe.png'), (16 * RESIZER, 32 * RESIZER)),
-            pygame.transform.scale(pygame.image.load('sword.png'), (16 * RESIZER, 32 * RESIZER))]
+handList = [pygame.transform.scale(pygame.image.load('empty.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('stick.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('spear.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('beer.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('pokeball.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('hammer.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('pickaxe.png'), (16 * resizer, 32 * resizer)),
+            pygame.transform.scale(pygame.image.load('sword.png'), (16 * resizer, 32 * resizer))]
 
-explosion = [pygame.transform.scale(pygame.image.load('explosion0.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
-             pygame.transform.scale(pygame.image.load('explosion1.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
-             pygame.transform.scale(pygame.image.load('explosion2.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
-             pygame.transform.scale(pygame.image.load('explosion3.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
-             pygame.transform.scale(pygame.image.load('explosion4.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
-             pygame.transform.scale(pygame.image.load('explosion5.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
-             pygame.transform.scale(pygame.image.load('explosion6.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
-             pygame.transform.scale(pygame.image.load('explosion7.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
-             pygame.transform.scale(pygame.image.load('explosion8.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3)))]
+explosion = [pygame.transform.scale(pygame.image.load('explosion0.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion1.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion2.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion3.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion4.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion5.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion6.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion7.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion8.png'), (int(21 * resizer / 1.3), int(21 * resizer / 1.3)))]
              
 
 bg = pygame.image.load('bg.png')
@@ -122,7 +121,7 @@ bg = pygame.image.load('bg.png')
 # --- Other variables
 showDebug = True
 
-stickList = [Stickman([100 - 7 * RESIZER, 100 - 12 * RESIZER], hatList[random.randint(0, len(hatList) - 1)], handList[random.randint(0, len(handList) - 1)])]
+stickList = [Stickman([100 - 7 * resizer, 100 - 12 * resizer], hatList[random.randint(0, len(hatList) - 1)], handList[random.randint(0, len(handList) - 1)])]
 
 animationList = []
 
@@ -143,7 +142,7 @@ while True:
             animationList.remove(animation)
     
     if showDebug == True:
-        debug = "Placeholder"
+        debug = ""
         debugText = basicFont.render(str(debug), True, YELLOW) #text | antialiasing | color
         windowSurface.blit(debugText, (1, 1))
 
@@ -151,9 +150,9 @@ while True:
     for event in pygame.event.get():
         if event.type == MOUSEBUTTONUP:
             if event.button == 1:
-                stickList.append(Stickman([mousePosition[0] - 7 * RESIZER, mousePosition[1] - 12 * RESIZER], hatList[random.randint(0, len(hatList) - 1)], handList[random.randint(0, len(handList) - 1)]))
+                stickList.append(Stickman([mousePosition[0] - 7 * resizer, mousePosition[1] - 12 * resizer], hatList[random.randint(0, len(hatList) - 1)], handList[random.randint(0, len(handList) - 1)]))
             if event.button == 3:
-                animationList.append(Explosion([mousePosition[0] - (21 * RESIZER / 2), mousePosition[1] - (21 * RESIZER / 2)]))
+                animationList.append(Explosion([mousePosition[0] - (21 * resizer / 2), mousePosition[1] - (21 * resizer / 2)]))
                 for stick in stickList:
                     if mousePosition[0] > stick.stickmanRect.left and mousePosition[0] < stick.stickmanRect.right and mousePosition[1] > stick.stickmanRect.top and mousePosition[1] < stick.stickmanRect.bottom:
                         stickList.remove(stick)
