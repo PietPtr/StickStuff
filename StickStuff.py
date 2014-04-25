0806f4beff5cb5d77c66998ea61be59a1f93d050
Pietdagamer <Pietdagamer@users.noreply.github.com>
2014-04-25 07:08:11 +0200

    Added explosion
    right click to eplode Stickmen and delete them

--- StickStuff.py
+++ StickStuff.py
@@ -47,7 +47,23 @@ class Stickman(object):
             self.reached = [False, False]
             self.lastMoveTime = pygame.time.get_ticks()
             self.waitTime = random.randint(3000, 20000)
-        
+
+class Explosion(object):
+    def __init__(self, position):
+        self.position = position
+        self.frame = 0
+        self.lastFrameTime = pygame.time.get_ticks()
+    def doTasks(self):
+        try:
+            windowSurface.blit(explosion[self.frame], (self.position[0], self.position[1]))
+        except IndexError:
+            return False
+        print pygame.time.get_ticks() - self.lastFrameTime
+        if pygame.time.get_ticks() - self.lastFrameTime >= 30:
+            self.frame = self.frame + 1
+            self.lastFrameTime = pygame.time.get_ticks()
+        return True
+      
 # --- Constants ---
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -90,12 +106,25 @@ handList = [pygame.transform.scale(pygame.image.load('empty.png'), (16 * RESIZER
             pygame.transform.scale(pygame.image.load('pickaxe.png'), (16 * RESIZER, 32 * RESIZER)),
             pygame.transform.scale(pygame.image.load('sword.png'), (16 * RESIZER, 32 * RESIZER))]
 
+explosion = [pygame.transform.scale(pygame.image.load('explosion0.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion1.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion2.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion3.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion4.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion5.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion6.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion7.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3))),
+             pygame.transform.scale(pygame.image.load('explosion8.png'), (int(21 * RESIZER / 1.3), int(21 * RESIZER / 1.3)))]
+             
+
 bg = pygame.image.load('bg.png')
 
 # --- Other variables
 showDebug = True
 
 stickList = [Stickman([100 - 7 * RESIZER, 100 - 12 * RESIZER], hatList[random.randint(0, len(hatList) - 1)], handList[random.randint(0, len(handList) - 1)])]
+
+animationList = []
 
 while True:
     frameTime = mainClock.tick(1000)
@@ -108,9 +137,13 @@ while True:
     for stick in stickList:
         stick.render()
         stick.move()
+
+    for animation in animationList:
+        if animation.doTasks() == False:
+            animationList.remove(animation)
     
     if showDebug == True:
-        debug = stickList[0].reached
+        debug = "Placeholder"
         debugText = basicFont.render(str(debug), True, YELLOW) #text | antialiasing | color
         windowSurface.blit(debugText, (1, 1))
 
@@ -120,10 +153,10 @@ while True:
             if event.button == 1:
                 stickList.append(Stickman([mousePosition[0] - 7 * RESIZER, mousePosition[1] - 12 * RESIZER], hatList[random.randint(0, len(hatList) - 1)], handList[random.randint(0, len(handList) - 1)]))
             if event.button == 3:
+                animationList.append(Explosion([mousePosition[0] - (21 * RESIZER / 2), mousePosition[1] - (21 * RESIZER / 2)]))
                 for stick in stickList:
                     if mousePosition[0] > stick.stickmanRect.left and mousePosition[0] < stick.stickmanRect.right and mousePosition[1] > stick.stickmanRect.top and mousePosition[1] < stick.stickmanRect.bottom:
-                        #add explosion to animation list (needs class)
-                        #delete character
+                        stickList.remove(stick)
         if event.type == KEYUP:
             if event.key == 284:
                 showDebug = not showDebug
